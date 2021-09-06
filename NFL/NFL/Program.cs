54b476805a5575f9572d512774c1d9fbebe8c957
r3surr3cti0n <81960019+r3surr3cti0n@users.noreply.github.com>
2021-09-06 10:53:09 +0200

    Add files via upload

--- NFL/NFL/Program.cs
+++ NFL/NFL/Program.cs
@@ -10,7 +10,6 @@ namespace NFL
     {
         static void Main(string[] args)
         {
-            
 
 
 
@@ -18,4 +17,47 @@ namespace NFL
             Console.ReadKey();
         }
     }
+
+
+        class Jatekos
+        {
+            public string Nev { get; set; }
+            public int Yardok { get; set; }
+            public int Kiserletek { get; set; }
+            public int Passzok { get; set; }
+            public int TD { get; set; }
+            public int Eladott { get; set; }
+            public double Iranymutato { get; set; }
+            public string Egyetem { get; set; }
+
+            public Jatekos(string sor)
+            {
+                string[] s = sor.Split(';');
+                Nev = s[0];
+                Yardok = Convert.ToInt16(s[1]);
+                Kiserletek = Convert.ToInt16(s[2]);
+                Passzok = Convert.ToInt16(s[3]);
+                TD = Convert.ToInt16(s[4]);
+                Eladott = Convert.ToInt16(s[5]);
+                Iranymutato = Convert.ToDouble(s[6]);
+                Egyetem = s[7];
+            }
+
+            private double Konvertal(string iranyitoMutato)
+            {
+                var decimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+                iranyitoMutato = iranyitoMutato.Replace(",", decimalSeparator).Replace(".", decimalSeparator);
+                if (double.TryParse(iranyitoMutato, out var ertek))
+                    return ertek;
+                throw new FormatException("Hibás érték (irányítómutató)");
+            }
+
+            public string FormazottNev(string nev)
+            {
+                var n = nev.Split(' ');
+                n[n.Length - 1] = n[n.Length - 1].ToUpper();
+                return string.Join(" ", n);
+            }
+
+        }
 }
