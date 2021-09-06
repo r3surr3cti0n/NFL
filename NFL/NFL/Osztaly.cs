897131693cb7c0b0f3201895fe74c512065dfb0f
r3surr3cti0n <81960019+r3surr3cti0n@users.noreply.github.com>
2021-09-06 10:02:27 +0200

    Add files via upload

---
+++ NFL/NFL/Osztaly.cs
@@ -0,0 +1,19 @@
+﻿class Jatekos
+{
+    private double Konvertal(string iranyitoMutato)
+    {
+        var decimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        iranyitoMutato = iranyitoMutato.Replace(",", decimalSeparator).Replace(".", decimalSeparator);
+        if (double.TryParse(iranyitoMutato, out var ertek))
+            return ertek;
+        throw new FormatException("Hibás érték (irányítómutató)");
+    }
+	
+	public string FormazottNev(string nev)
+	{
+		var n = nev.Split(' ');
+		n[n.Length - 1] = n[n.Length - 1].ToUpper();
+		return string.Join(" ", n);
+	}
+
+}
